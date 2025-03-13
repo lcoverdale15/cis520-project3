39303f321454f45f1c22157f4b42eadbdf56585e
Tatenda Sekabanja <sekabanj@icr-clymene.beocat.ksu.edu>
2025-03-13 11:24:14 -0500

    m

--- src/block_store.c
+++ src/block_store.c
@@ -9,7 +9,7 @@
 struct block_store 
 {
 	uint8_t blocks[BLOCK_STORE_NUM_BLOCKS][BLOCK_SIZE_BYTES];
-	uint8_t *bitmap;
+	uint8_t  *bitmap;
 	uint8_t *secondHalf; 
 };
 
@@ -40,11 +40,44 @@ void block_store_destroy(block_store_t *const bs)
 		free(bs);
 	}
 }
-
+/*remember, parameter validation, error checking, comments
+ This function finds the first free block in the block store and marks it as allocated in the bitmap.
+  It returns the index of the allocated block or SIZE_MAX if no free block is available.
+*/
 size_t block_store_allocate(block_store_t *const bs)
 {
-	UNUSED(bs);
-	return 0;
+	if(bs == NULL || bs->blocks == NULL || bs->bitmap == NULL){
+		//review this, unsigned 
+		return SIZE_MAX;
+	}
+	uint8_t *bitmap = bs->bitmap;
+	//go over all the bitmaps
+	for(int i = 0; i<BITMAP_NUM_BLOCKS ;i++){
+		//go over every byte in a block 
+		for(int byte = 0; byte < BLOCK_SIZE_BYTES;byte++){
+		//go over every bit in the byte
+			for(int bit = 0; bit <8 ; bit ++){
+				uint8_t flag = 1;
+				//shift the flag to the correct bit
+				flag = flag << bit;
+				//see if the bit is set
+				uint8_t new_data = bitmap[byte] & flag;
+				//there are only 510 available storage blocs
+				// if(i == BITMAP_NUM_BLOCKS-1 && byte == BLOCK_SIZE_BYTES-1 && bit ==6){
+				// 	return SIZE_MAX;
+				// }
+				if(new_data ==0){
+					//set the data
+					new_data = bitmap[byte] | flag;
+					bitmap[byte] = new_data;
+					// printf("%d\n",(i * BLOCK_SIZE_BYTES * 8) + byte * 8 + bit);
+					return (i *BLOCK_SIZE_BYTES * 8) + byte * 8 + bit;
+				}
+			}
+		}
+			bitmap += BLOCK_SIZE_BYTES;	
+		}
+		return SIZE_MAX;
 }
 
 /*
@@ -63,6 +96,7 @@ bool block_store_request(block_store_t *const bs, const size_t block_id)
 	size_t byte_index = block_id / 8;
 	size_t bit_index = block_id % 8;
 	uint8_t mask = 1 << bit_index; //creating bitmask at posisiton bit_index
+	
 
 	if((bs->bitmap[byte_index] & mask) != 0){
 		return false;
@@ -71,12 +105,40 @@ bool block_store_request(block_store_t *const bs, const size_t block_id)
 	bs->bitmap[byte_index] |= mask;
 
 	return (bs->bitmap[byte_index] & mask);
-}
+	//find the first free block, set it to used, return that index
 
+	}
+
+/*This function marks a specific block as free in the bitmap. It first checks if the pointer to the block store is
+ not NULL and if the block_id is within the range of valid block indices. Then, it resets the bit corresponding to 
+ the block in the bitmap.
+ */
 void block_store_release(block_store_t *const bs, const size_t block_id)
 {
-	UNUSED(bs);
-	UNUSED(block_id);
+			if(bs == NULL || bs->blocks == NULL || bs->bitmap == NULL){
+				return  ;
+			}
+			if((block_id>=BITMAP_START_BLOCK+BITMAP_NUM_BLOCKS) || ((int)block_id<BITMAP_START_BLOCK)){
+				return  ;
+			}
+
+			//find the bit, reset it 
+			// int bitmapIndex = block_id / (BLOCK_SIZE_BYTES * 8 -1);
+			//uint8_t * bitmap = bs->bitmap[bitmapIndex];
+			uint8_t * bitmap = bs->bitmap;
+			
+			size_t byte_index = block_id / 8;
+			size_t bit_index = block_id % 8;
+			   uint8_t  flag = 1;
+			//shift the flag to the correct position
+			flag = flag << bit_index;
+			//flip the flag
+			flag = ~flag;
+			//reset the bit to zero
+			uint8_t new_data = bitmap[byte_index] & flag;
+			bitmap[byte_index] = new_data; 
+			return   ;
+	
 }
 
 size_t block_store_get_used_blocks(const block_store_t *const bs)
